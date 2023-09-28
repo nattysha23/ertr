7f7a11828e420c5de2332834b206e6f9e1d93e2f
Natalya <nattysha@mail.ru>
2023-09-28 11:36:32 +0300

    logs is here F:\\Transcend\\4

--- nat/Program.cs
+++ nat/Program.cs
@@ -11,7 +11,7 @@ try
 
 
     // Add services to the container.
-
+    //
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
