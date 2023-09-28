3a08778819752a17cac3da50c0e1998a3f21fc26
Natalya <nattysha@mail.ru>
2023-09-28 12:54:23 +0300

    not all 3lb

---
+++ nat/Database/StudentDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace nat.Database
+{
+    public class StudentDbContext: DbContext
+    {
+public StudentDbContext(DbContextOptions<StudentDbContext> options): base(options)
+        { 
+        }  
+    }
+}
