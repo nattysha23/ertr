5781263ae166918243b8fd058f3e988c945360e9
Natalya <nattysha@mail.ru>
2023-09-28 11:30:13 +0300

    Get, Post done

--- nat/Controllers/WeatherForecastController.cs
+++ nat/Controllers/WeatherForecastController.cs
@@ -30,5 +30,14 @@ namespace nat.Controllers
             })
             .ToArray();
         }
+        [HttpPost(Name = "AddNewSummary")]
+        public string[] AddNewSummary(string newSummary)
+        {
+            _logger.LogError("New method was called");
+            var list = Summaries.ToList();
+            list.Add(newSummary);
+            return list.ToArray();
+            
+        }
     }
 }
