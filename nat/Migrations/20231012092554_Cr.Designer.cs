208a5e0489ad1af794d9576a4303872ea4871ca3
Petrova Valentina <valya@chuvsu.ru>
2023-10-12 12:30:52 +0300

    3 done

--- nat/Migrations/20231012092554_Cr.Designer.cs
+++ nat/Migrations/20231012092554_Cr.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using nat.Database;
 
@@ -10,9 +11,11 @@ using nat.Database;
 namespace nat.Migrations
 {
     [DbContext(typeof(StudentDbContext))]
-    partial class StudentDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231012092554_Cr")]
+    partial class Cr
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
